3a074cb8221a359816728926e7346a07433e5f6f
Keerthana Mahathi <keerthana@example.com>
2025-11-07 23:18:49 +0530

    Fixed API URL to use relative path for Render deployment

--- health-frontend/src/App.js
+++ health-frontend/src/App.js
@@ -127,7 +127,8 @@ function App() {
     setPrediction(null);
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      //const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch("/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
